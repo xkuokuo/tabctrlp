bef53d8f5ad8421adde786ef689cec98243b35ff
Xin <kuoxinkuoxin@gmail.com>
2016-11-20 22:06:53 -0800

    Use reactJS for tabs display board

--- src/js/menu.jsx
+++ src/js/menu.jsx
@@ -3,12 +3,6 @@ var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var currentTabIndex = 0;
-chrome.tabs.getSelected(function(tab){
-    //TODO: getSelected methos is deprecated. Replace it
-    currentTabIndex = tab.index;
-});
-
 function getAllTabsOfCurrentWindow(callback) {
     getAllTabs(chrome.windows.WINDOW_ID_CURRENT, callback);
 }
@@ -19,90 +13,95 @@ function getAllTabs(windowId, callback) {
     });
 }
 
-function displayText(text) {
-    document.getElementById('status').textContent = text;
-}
+class TabEntry extends React.Component {
 
-function addTabEntry(Tab) {
-    $('ul').append(createTabEntry(Tab));
-}
-
-function createTabEntry(Tab) {
-    var entry = $('<li></li>');
-    entry.addClass('clearfix');
-    if (Tab.index == currentTabIndex) {
-        entry.addClass('tab-selected');
-    }
-    var favIcon = $('<img class="img-responsive pull-left" src=' + Tab.favIconUrl + '>');
-    var tabTitle = $('<p></p>');
-    tabTitle.addClass('tabTitle');
-    var tabID = $('<p></p>');
-    tabID.text(Tab.id);
-    tabID.addClass('tabID');
-    tabID.addClass('hidden');
-    tabTitle.text(Tab.title);
-    entry.append(favIcon);
-    entry.append(tabTitle);
-    entry.append(tabID);
-    return entry;
-}
-
-function moveUp() {
-    var currentSelected = $('.tab-selected');
-    var newSelected = currentSelected.prev('li');
-    if (newSelected.length == 1) {
-        newSelected.addClass('tab-selected');
-        currentSelected.removeClass('tab-selected');
+    render() {
+        return (
+            <li className={this.props.selected ? "clearFix tab-selected" : "clearFix"}>
+                <img className="img-responsive pull-left" src={this.props.tab.favIconUrl}/>
+                <p className="tabTitle">{this.props.tab.title}</p>
+                <p className="tabID hidden">{this.props.tab.id}</p>
+            </li>);
     }
 }
 
-function moveDown() {
-    var currentSelected = $('.tab-selected');
-    var newSelected = currentSelected.next('li');
-    if (newSelected.length == 1) {
-        newSelected.addClass('tab-selected');
-        currentSelected.removeClass('tab-selected');
+class TabsDisplay extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {allTabs: [], matchedTabsIndex: [], selectedTabIndex: 0};
+        getAllTabsOfCurrentWindow((returnedTabs) => {
+            this.setState({allTabs: returnedTabs});
+        });
+        chrome.tabs.getSelected((tab) => {
+            //TODO: getSelected methos is deprecated. Replace it
+            this.setState({selectedTabIndex: tab.index});
+        });
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    handleKeyDown(e){
+        if (e.keyCode == 38) {
+            this.moveUp();
+        }else if (e.keyCode == 40) {
+            this.moveDown();
+        }else if (e.keyCode == 13) {
+            this.jumpToTab(this.getSelectedTabID());
+        }
+    }
+
+    getSelectedTabID(){
+        return this.state.allTabs[this.state.selectedTabIndex].id;
+    }
+
+    moveDown(){
+        this.setState((prevState, props) => {
+            var newIndex = prevState.selectedTabIndex + 1;
+            return {selectedTabIndex: newIndex>=this.state.allTabs.length ? 0 : newIndex};
+        });
+    }
+
+    moveUp(){
+        this.setState((prevState, props) => {
+            var newIndex = prevState.selectedTabIndex - 1;
+            return {selectedTabIndex: newIndex<0 ? this.state.allTabs.length-1 : newIndex};
+        });
+    }
+
+    jumpToTab(tabID){
+        chrome.tabs.update(tabID, {active: true, highlighted:true});
+    }
+
+    render() {
+        var tabEntrys =  R.map((tab) =>
+            <TabEntry key={tab.id} tab={tab} selected={tab.index===this.state.selectedTabIndex?true:false}/>,
+            this.state.allTabs);
+        return (
+            <div onKeyDown={this.handleKeyDown}>
+                {this.props.children}
+                <ul className="tab-list list-unstyled clear-fix">
+                    {tabEntrys}
+                </ul>
+            </div>);
     }
 }
 
-function getSelectedTabID() {
-    return  Number($.trim($('.tab-selected .tabID').text()));
-}
-
-function appendToInputDisplay(char) {
-    $('.input-display').append(char);
-}
-
-function deleteFromInputDisplay(char) {
-    $('.input-display').append(char);
-}
-
-//Bind the Up and Down button
-$(document).keydown(function(e){
-    if (e.keyCode == 38) {
-        moveUp();
-    }else if (e.keyCode == 40) {
-        moveDown();
-    }else if (e.keyCode == 13) {
-        chrome.tabs.update(getSelectedTabID(), {active: true, highlighted:true});
-    }
-    /*
-    else {
-        appendToInputDisplay(String.fromCharCode(e.keyCode));
-    }
-    */
-})
 
 //input display div box
 class InputDisplay extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {input: 'PlaceHolder'};
+        this.state = {input: ''};
         this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleKeyPress(e){
         var inputChar = String.fromCharCode(e.charCode);
+        this.setState((prevState, props) => ({
+            input: prevState.input + inputChar
+        }));
+    }
+
+    appendToDisplay(char){
         this.setState((prevState, props) => ({
             input: prevState.input + inputChar
         }));
@@ -115,11 +114,8 @@ class InputDisplay extends React.Component {
 
 //Display all tabs
 document.addEventListener('DOMContentLoaded', function() {
-    getAllTabsOfCurrentWindow(function(resultTabs){
-        R.map(addTabEntry, resultTabs);
-    });
     ReactDOM.render(
-        <InputDisplay/>,
+        <TabsDisplay><InputDisplay/></TabsDisplay>,
         document.getElementById('root')
     );
 });
